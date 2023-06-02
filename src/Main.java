910eaab07a2f15b84e8376a33016b0c46ac74d38
Wladimir <weizen.w@gmx.de>
2023-06-02 11:24:59 +0200

    feat: add method Main.indexOfRevers and solution to task1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,7 +8,14 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     List<Integer> numbers = readList(scanner);
-
+    Collections.sort(numbers);
+    Collections.reverse(numbers);
+    System.out.print("Введите элемент для поиска: ");
+    int x = scanner.nextInt();
+    scanner.nextLine();
+    int i = indexOfRevers(numbers, x);
+    System.out.println("Индекс искомого элемента: " + i);
+    System.out.println("Сорт.список: " + numbers);
   }
 
   private static List<Integer> readList(Scanner scanner) {
@@ -21,5 +29,25 @@ public class Main {
       scanner.nextLine();
     }
     return list;
+  }
+
+  private static int indexOfRevers(List<Integer> list, int target) {
+    int left = 0;
+    int right = list.size();
+    while (left < right - 1) {
+      int indexMid = (left + right) / 2;
+      int mid = list.get(indexMid);
+      if (mid > target) {
+        left = mid + 1;
+      } else if (mid < target) {
+        right = mid - 1;
+      } else {
+        return indexMid;
+      }
+    }
+    if (list.get(left) == target) {
+      return left;
+    }
+    return -1;
   }
 }
